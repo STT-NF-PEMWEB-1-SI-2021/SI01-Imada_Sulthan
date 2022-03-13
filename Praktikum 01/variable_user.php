52b39c735e3aea26ebe7b6c1e020c40604f4b74b
Imada_Sulthan <92011946+ImadaSulthan@users.noreply.github.com>
2022-03-13 15:03:33 +0700

    Add files via upload

---
+++ Praktikum 01/variable_user.php
@@ -0,0 +1,13 @@
+<?php
+    // mendefinisikan variable
+    $nama = 'Imada Sulthan';
+    $umur = 19;
+    $berat = 70;
+
+    echo 'Nama : ' . $nama;
+    echo '<br/>Umur : ' . $umur. ' Tahun';
+    echo '<br/> Berat : ' . $berat. ' Kg';
+
+    echo "<br/><br/>Hello $nama, apa kabar?";
+
+?>
