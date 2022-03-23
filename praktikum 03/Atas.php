e7203df074bf5f380abd22adbc12fe30d35cf3d0
Imada_Sulthan <92011946+ImadaSulthan@users.noreply.github.com>
2022-03-23 16:58:04 +0700

    Add files via upload

---
+++ praktikum 03/Atas.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>SAS-STT NF</title>
+    </head>
+    <body>
+        <header role="banner">
+        <h2 class="text center">Student Activity Score - STTNF</h2><hr/>
+    </header>
---
+++ praktikum 03/Bawah.php
@@ -0,0 +1,8 @@
+<footer>
+    <p>
+        Created by <a href="http://nurulfikri.ac.id">Pus info NF&copy;2017</a>
+    </p>
+</footer>
+
+</body>
+</html>
